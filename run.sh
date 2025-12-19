6c5b9def967e82902c0a4e949cccb3d118bca08c
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-19 16:17:07 +0530

    terraform roboshop final v1

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 if [ "$1" == "dev" ]; then
   rm -f .terraform/terraform.tfstate
   terraform init -backend-config=env-dev/state.tfvars
-  if [ "$2"=="destroy" ]; then
+  if [ "$2" == "destroy" ]; then
     terraform destroy -auto-approve -var-file=env-dev/main.tfvars
   else
     terraform apply -auto-approve -var-file=env-dev/main.tfvars
@@ -11,7 +11,7 @@ fi
 if [ "$1" == "prod" ]; then
   rm -f .terraform/terraform.tfstate
   terraform init -backend-config=env-prod/state.tfvars
-  if [ "$2"=="destroy" ]; then
+  if [ "$2" == "destroy" ]; then
     terraform destroy -auto-approve -var-file=env-prod/main.tfvars
   else
     terraform apply -auto-approve -var-file=env-prod/main.tfvars
