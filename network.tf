e7e5f0f36af4cd3e4f1538716026418c7649eccb
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-21 12:02:08 +0530

    terraform roboshop final v1

--- network.tf
+++ network.tf
@@ -1,24 +1,29 @@
 resource "aws_security_group" "ex2" {
-    for_each            = var.components
-    name                = "${each.key}-${var.env}"
-    description         = "${each.key}-${var.env}"
+  for_each            = var.components
+  name                = "${each.key}-${var.env}"
+  description         = "${each.key}-${var.env}"
 
-    egress {
-        from_port       = 0
-        to_port         = 0
-        protocol        = "-1"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+  egress {
+      from_port       = 0
+      to_port         = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
+  }
 
-    dynamic "ingress" {
-        for_each        = each.value["ports"]
+  dynamic "ingress" {
+      for_each        = each.value["ports"]
 
-        content {
-          from_port     = ingress.value
-          to_port       = ingress.value
-          protocol      = "tcp"
-          cidr_blocks   = ["0.0.0.0/0"]
-          description   = ingress.key
-        }
-    }
+      content {
+        from_port     = ingress.value
+        to_port       = ingress.value
+        protocol      = "tcp"
+        cidr_blocks   = ["0.0.0.0/0"]
+        description   = ingress.key
+      }
+  }
+
+  tags = {
+    Name = "${each.key}-${var.env}"
+  }
+
 }
