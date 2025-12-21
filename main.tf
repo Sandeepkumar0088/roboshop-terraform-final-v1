4bd5bcfbc13f8e939dc184f20d1c549c29c35c05
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2025-12-21 11:41:47 +0530

    terraform roboshop final v1

--- main.tf
+++ main.tf
@@ -3,21 +3,21 @@ resource "aws_instance" "instance" {
   for_each                = var.components
 
   ami                     = var.ami
-  instance_type           = var.instance_type
+  instance_type           = each.value["instance_type"]
   vpc_security_group_ids  = [ aws_security_group.ex2[each.key].id ]
 
   tags = {
-    Name                  = "${each.key}-${var.env}"
+    Name    = "${each.key}-${var.env}"
   }
 }
 
 resource "aws_route53_record" "dns_records" {
-  for_each                = var.components
-  zone_id                 = var.zone_id
-  name                    = "${each.key}-${var.env}"
-  type                    = "A"
-  ttl                     = 30
-  records                 = [aws_instance.instance[each.key].private_ip]
+  for_each  = var.components
+  zone_id   = var.zone_id
+  name      = "${each.key}-${var.env}"
+  type      = "A"
+  ttl       = 30
+  records   = [aws_instance.instance[each.key].private_ip]
 }
 
 
